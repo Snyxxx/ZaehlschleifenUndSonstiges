7119edd687eb51e8f38f0a650a7c7a7ef0cccff1
Snyxxx <samueleulberg@gmail.com>
2023-04-18 22:32:27 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        printSomeNumbers();
+    }
+
+    public static void printSomeNumbers(){
+        for (int i = 6; i <= 26; i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void printNumbers(int start, int end){
+
+    }
+
+    public static void sumUp(int start, int end) {
+
+    }
+}
